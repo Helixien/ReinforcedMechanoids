ada7bd89791b24442d732683990dec0070359870
Taranchuk <nfhfyxer@gmail.com>
2022-02-10 18:18:41 +0200

    update

--- 1.3/Source/ReinforcedMechanoids/ReinforcedMechanoids/CompChangePawnGraphic.cs
+++ 1.3/Source/ReinforcedMechanoids/ReinforcedMechanoids/CompChangePawnGraphic.cs
@@ -34,19 +34,17 @@ namespace ReinforcedMechanoids
         public void TryChangeGraphic(bool changeAnyWay)
         {
             var pawn = this.parent as Pawn;
+            Log.Message(pawn + " - " + this.parent?.GetType() + " - " + Props + " - " + Props?.pawnGraphicsByMissingParts);
             foreach (var pawnGraphic in Props.pawnGraphicsByMissingParts)
             {
                 var bodyPart = Core.GetNonMissingBodyPart(pawn, pawnGraphic.missingPart);
-                Log.Message("Found part: " + bodyPart + " - " + this.currentTexPath + " - " + pawnGraphic.texPath);
                 if (bodyPart != null)
                 {
-                    Log.Message("Changing now: " + pawn.ageTracker.CurKindLifeStage.bodyGraphicData.texPath);
                     ChangeGraphic(pawn, pawn.ageTracker.CurKindLifeStage.bodyGraphicData.texPath);
                     return;
                 }
                 else if (changeAnyWay || pawnGraphic.texPath != this.currentTexPath)
                 {
-                    Log.Message("Changing now: " + pawnGraphic.texPath);
                     ChangeGraphic(pawn, pawnGraphic.texPath);
                     return;
                 }
@@ -57,10 +55,9 @@ namespace ReinforcedMechanoids
         {
             LongEventHandler.ExecuteWhenFinished(delegate
             {
-                Log.Message("Changing graphic to " + texPath);
                 Graphic_Multi nakedGraphic = (Graphic_Multi)GraphicDatabase.Get<Graphic_Multi>(texPath, ShaderDatabase.Cutout, pawn.ageTracker.CurKindLifeStage.bodyGraphicData.drawSize, Color.white);
-                PawnRenderer.graphics.nakedGraphic = nakedGraphic;
                 PawnRenderer.graphics.ResolveAllGraphics();
+                PawnRenderer.graphics.nakedGraphic = nakedGraphic;
                 currentTexPath = texPath;
             });
         }
---
+++ 1.3/Source/ReinforcedMechanoids/ReinforcedMechanoids/CompExplodeIfNoOtherFactionPawns.cs
@@ -0,0 +1,65 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace ReinforcedMechanoids
+{
+    public class CompProperties_ExplodeIfNoOtherFactionPawns : CompProperties
+    {
+        public float radius;
+        public CompProperties_ExplodeIfNoOtherFactionPawns()
+        {
+            this.compClass = typeof(CompExplodeIfNoOtherFactionPawns);
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public class CompExplodeIfNoOtherFactionPawns : ThingComp
+    {
+        public CompProperties_ExplodeIfNoOtherFactionPawns Props => base.props as CompProperties_ExplodeIfNoOtherFactionPawns;
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            if (parent is Pawn pawn)
+            {
+                if (pawn.equipment is null)
+                {
+                    pawn.equipment = new Pawn_EquipmentTracker(pawn);
+                }
+                if (pawn.apparel is null)
+                {
+                    pawn.apparel = new Pawn_ApparelTracker(pawn);
+                }
+            }
+            this.TryExplode();
+        }
+
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (Find.TickManager.TicksGame % 60 == 0)
+            {
+                TryExplode();
+            }
+        }
+        public void TryExplode()
+        {
+            var pawn = this.parent as Pawn;
+            if (pawn.Map != null && pawn.Faction != null)
+            {
+                if (!pawn.Map.mapPawns.SpawnedPawnsInFaction(pawn.Faction).Where(x => x.kindDef != pawn.kindDef).Any())
+                {
+                    GenExplosion.DoExplosion(pawn.Position, pawn.Map, Props.radius, DamageDefOf.Bomb, pawn);
+                    if (!pawn.Dead)
+                    {
+                        pawn.Kill(null);
+                    }
+                }
+            }
+        }
+
+    }
+}
