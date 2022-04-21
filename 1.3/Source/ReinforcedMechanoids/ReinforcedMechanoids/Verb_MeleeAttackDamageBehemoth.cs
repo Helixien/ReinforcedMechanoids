bae2f9d4c2c3f2c595b62899b1f344e674e9d1f2
Taranchuk <nfhfyxer@gmail.com>
2022-04-21 16:18:27 +0300

    updated the code to use the pushback code from VEF

---
+++ 1.3/Source/ReinforcedMechanoids/ReinforcedMechanoids/Verb_MeleeAttackDamageBehemoth.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System;
+using Verse;
+using RimWorld;
+
+namespace ReinforcedMechanoids
+{
+    public class Verb_MeleeAttackDamageBehemoth : Verb_MeleeAttackDamage
+    {
+        public override bool Available()
+        {
+            return Core.GetNonMissingBodyPart(CasterPawn, RM_DefOf.RM_BehemothShield) != null && base.Available();
+        }
+    }
+}
